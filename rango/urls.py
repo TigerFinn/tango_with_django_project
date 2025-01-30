c66899776fdc13a97e34c7e0ff7a20ab8a749b2b
Finlay Murphy <2765063m@student.gla.ac.uk>
2025-01-30 14:35:23 +0000

    Chapter 4 commit

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,7 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
+    path('tiger/', views.tiger, name = 'tiger'),
+    path('tiger2/', views.tiger2, name = 'tiger2'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Rango says hey there partner!")
+from random import choice
 
 # Create your views here.
+
+def index(request):
+    context_dict = {"boldmessage":"Crunchy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {"boldmessage":"This page has been put together by Finlay"}
+    
+    return render(request, 'rango/about.html', context=context_dict)
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+def tiger(request):
+    tigerAttributes = ["Majestic","The Best","Orange","Better than Kapaburys"]
+    context_dict = {"tigerMessage":choice(tigerAttributes), "finalMessage":"And in case I don't see you.... Good afternoon, good evening and goodnight"}
+
+    return render(request, 'rango/tiger.html', context=context_dict)
+
+def tiger2(request):
+    context_dict = {"tigerMessage":"amazing and there's nothing else to say", "finalMessage":"I said there's nothing else to say"}
+    return render(request, 'rango/tiger.html', context = context_dict)
