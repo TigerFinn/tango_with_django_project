02189150c6998091fb741eb6cd015bff96eea2ed
Finlay Murphy <2765063m@student.gla.ac.uk>
2025-02-03 11:36:01 +0000

    chapter 5 commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PageOnCategory(admin.TabularInline):
+    model = Page
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Category Name', {'fields':['name']}),
+        ('Interaction Information', {'fields':['likes','views']})
+    ]
+    inlines = [PageOnCategory]
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -11,8 +11,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {"boldmessage":"This page has been put together by Finlay"}
-    
+    context_dict = {"boldmessage":"Finlay"}
+
     return render(request, 'rango/about.html', context=context_dict)
     #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
