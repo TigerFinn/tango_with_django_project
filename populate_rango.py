5b3396582d08d4e6d34500fe439e75b5a705fc79
Finlay Murphy <2765063m@student.gla.ac.uk>
2025-02-04 12:31:06 +0000

    Chapter 6 with everything but the Django Tutorial Pt. 3

--- populate_rango.py
+++ populate_rango.py
@@ -9,20 +9,20 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-        {'title':'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/'}
+        {'title':'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/', 'views':16},
+        {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/', 'views':5},
+        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/', 'views':5}
     ]
 
     django_pages = [
-        {'title':'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django','url':'http://www.tangowithdjango.com/'}
+        {'title':'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':12},
+        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views':1},
+        {'title':'How to Tango with Django','url':'http://www.tangowithdjango.com/', 'views':2}
     ]
 
     other_pages = [
-        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask','url':'http://flask.pocoo.org'}
+        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/', 'views':14},
+        {'title':'Flask','url':'http://flask.pocoo.org', 'views':3}
     ]
 
     # tiger_pages = [
@@ -38,7 +38,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data)
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
     
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/admin.py
+++ rango/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+#A class that allows pages to be displayed whilst editing a category
 class PageOnCategory(admin.TabularInline):
     model = Page
 
 
 
+#Customisation of split (sections), prepopulation and the displaying of pages#
+# of the category admin page
 class CategoryAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Category Name', {'fields':['name']}),
-        ('Interaction Information', {'fields':['likes','views']})
+        ('Interaction Information', {'fields':['likes','views']}),
+        ('URL Slug', {'fields':['slug']})
     ]
+    prepopulated_fields = {'slug':('name',)}
+    
     inlines = [PageOnCategory]
 
 
+#Customisation of the order of the page admin
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
 
